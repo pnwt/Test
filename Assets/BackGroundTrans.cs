e1878ad25b3ee2f19ea558a097ebb9e16f3a4efa
Stamp <thanapoom.dec@sbacnon.ac.th>
2023-02-16 21:36:35 +0700

    fix bug

--- Assets/BackGroundTrans.cs
+++ Assets/BackGroundTrans.cs
@@ -7,14 +7,13 @@ public class BackGroundTrans : MonoBehaviour
 
     private void Update()
     {
-        if(Ground.WaveCount == Ground.EndPhase && transform.position.x > -5.7f)
+        if(Ground.Instance.WaveCount == Ground.Instance.EndPhase && transform.position.x > -5.7f)
         {
             transform.Translate(-transform.right * 10 * Time.deltaTime);
         }
         else if (transform.position.x <= -5.7f)
         {
-            Ground.WaveCount = 0;
-            Ground.EndPhase = int.MaxValue;
+            Ground.Instance.EndPhase = int.MaxValue;
         }
     }
 }
--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    public static Ground Instance { get; private set; }
     [SerializeField] private float _GroundStartSpeed = 10f;
     [SerializeField] private GameObject _PhaseOne;
     [SerializeField] private GameObject _PhaseTwo;
     private float _Multiplier;
     private bool _GameOver = false;
-    public static int WaveCount = 0;
-    public static int EndPhase = 2;
+    public int WaveCount = 0;
+    public int EndPhase = 2;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     private void Start()
     {
         Player.OnGameOver += () => _GameOver = true;
@@ -20,10 +31,12 @@ public class Ground : MonoBehaviour
         {
             transform.position = new Vector3(37, transform.position.y, transform.position.z);
             WaveCount++;
+            Debug.Log($"Wave count = {WaveCount}/{EndPhase}");
         }
-        if (!_GameOver && WaveCount < EndPhase) transform.Translate(-transform.right * _GroundStartSpeed * ((_Multiplier >= 1) ? _Multiplier : 1) * Time.deltaTime);
+        if (_GameOver == false && WaveCount < EndPhase) transform.Translate(-transform.right * _GroundStartSpeed * ((_Multiplier >= 1) ? _Multiplier : 1) * Time.deltaTime);
         else if(WaveCount >= EndPhase) 
         {
+            Debug.Log($"Wave count = {WaveCount}/{EndPhase}\nEndPhase");
             Destroy(_PhaseOne);
             _PhaseTwo.SetActive(true);
         }
